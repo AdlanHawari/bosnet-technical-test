4b7d7d6ee560980833068e9fcb12cc26516b97a7
Adlan Hawari <hwr.adlan@gmail.com>
2022-10-20 16:05:35 +0700

    commit solution 5

--- pages/5/index.js
+++ pages/5/index.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function NumberFivePage() {
   const input = [1, 2, 4, 5, 9];
   const sum = 7;
+
+  function Operate(sum, input) {
+    input.forEach((item, index) => {
+      let arr = [];
+      if (sum - item > 0) {
+        // for (let i = index; i < input.length; i++) {
+        //   if (sum - item - index[i] >= 0) {
+        //     console.log("conatin:", index[i]);
+        //   }
+        // }
+      }
+    });
+  }
+
+  useEffect(() => {
+    Operate(sum, input);
+    // console.log("bound");
+    // console.log(a);
+  }, []);
+
   return <div>index</div>;
 }
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ export default function HomePage() {
     { title: "2", route: "/2" },
     { title: "3", route: "/3" },
     { title: "4", route: "/4" },
+    { title: "5", route: "/5" },
+    { title: "6", route: "/6" },
   ];
   const router = useRouter();
   return (
