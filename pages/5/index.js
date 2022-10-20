bb5aa9110497195763aaddb44128a5408407f1e1
Adlan Hawari <hwr.adlan@gmail.com>
2022-10-20 15:41:13 +0700

    home page

---
+++ pages/5/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function NumberFivePage() {
+  const input = [1, 2, 4, 5, 9];
+  const sum = 7;
+  return <div>index</div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,32 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function HomePage() {
-  return <div>index</div>;
+  const page = [
+    { title: "1", route: "/1" },
+    { title: "2", route: "/2" },
+    { title: "3", route: "/3" },
+    { title: "4", route: "/4" },
+  ];
+  const router = useRouter();
+  return (
+    <div className="py-10 flex justify-center w-full">
+      <div className="space-y-10">
+        <h1>Answer List:</h1>
+
+        <ul className="space-y-6">
+          {page.map((item, index) => (
+            <li key={index}>
+              <button
+                className="bg-slate-600 px-10 rounded-lg py-2 text-white"
+                onClick={() => router.push(item.route)}
+              >
+                {item.title}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
